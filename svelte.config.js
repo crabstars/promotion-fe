12e9a89cbff412b785e86884e6367bf1258d0f2e
kami <danielwaechtler@protonmail.com>
2023-09-17 20:11:41 +0200

    change 200 to index

--- svelte.config.js
+++ svelte.config.js
@@ -12,13 +12,10 @@ const config = {
 		inspector: true,
 	},
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: "200.html",
+			fallback: "index.html",
 			precompress: false,
 			strict: true
 		})
